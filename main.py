16f7318c8f33c463f1972c951d0cb65b60777a96
Jon Wayne Parrott <jjramone13@gmail.com>
2014-10-01 22:17:59 -0400

    Initial example - Added ferris as a dependency. - Added guestbook example code. - Added searchability.

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
 """`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
+# Import Ferris so we get fun stuff.
+import ferris3
+
+# Import the app engine datastore api
+from google.appengine.ext import ndb
+
+
+class GuestbookPost(ferris3.ndb.Model):
+    # Using ferris3.ndb.Model gives up behaviors.
+    class Meta:
+        # We'll use the searchable behavior to automatically index our models.
+        behaviors = (ferris3.search.Searchable,)
+
+    content = ndb.TextProperty()
+    author = ndb.UserProperty(auto_current_user_add=True, indexed=False)
+    created = ndb.DateTimeProperty(auto_now_add=True)
+
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+def guestbook_list():
+    greetings = GuestbookPost.query().order(-GuestbookPost.created)
+    return render_template("guestbook.html", greetings=greetings)
+
+
+@app.route('/search')
+def guestbook_search():
+    query = request.args.get('query', '')
+    search_results = ferris3.search.search('searchable:GuestbookPost', query)
+    greetings = ferris3.search.to_entities(search_results).items
+    return render_template('guestbook.html', greetings=greetings, query=query)
+
+
+@app.route('/sign', methods=['POST'])
+def guestbook_sign():
+    content = request.form.get('content')
+    post = GuestbookPost(content=content)
+    post.put()
+    return redirect('/')
 
 
 @app.errorhandler(404)
